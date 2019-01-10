293426a0a944c6a0663f308c5b99f2c80c1b4d8e
gshau <gshaughnessy@me.com>
2019-01-09 23:35:04 -0600

    fixed typo in start script

--- start-server.sh
+++ start-server.sh
@@ -4,4 +4,5 @@
 # http://www.raspberrypi-spy.co.uk/2014/05/how-to-autostart-apps-in-rasbian-lxde-desktop/
 
 echo "Starting Relay Controller server process"
-python3 server.py runserver -r
+# python3 server.py runserver -r
+/usr/bin/python3 /home/pi/pi-relay-controller-modmypi/server.py runserver -r
